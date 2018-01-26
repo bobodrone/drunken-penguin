3386e604c8b8b1708819d6f5f7c630f3bd15449a
Fredric Bergström <fredric@bobodrone.com>
2018-01-26 23:11:19 +0100

    added dropcat

--- parts/drupal.sh
+++ parts/drupal.sh
@@ -5,3 +5,10 @@ if [ ! -f /usr/local/bin/drush ]; then
     sudo wget -O /usr/local/bin/drush https://github.com/drush-ops/drush-launcher/releases/download/0.4.2/drush.phar
     sudo chmod +x /usr/local/bin/drush
 fi
+
+
+# install dropcat
+curl -OL h https://dropcat.org/dropcat.phar > dropcat.phar
+chmod +x dropcat.phar
+sudo mv dropcat.phar /usr/local/bin/dropcat
+dropcat about
