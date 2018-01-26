149071b50f7d328606d7ad39145b9f870b1ba8bc
Fredric Bergström <fredric@bobodrone.com>
2018-01-26 13:50:26 +0100

    added docker-compose and fixed -y on signal

--- parts/leisure.sh
+++ parts/leisure.sh
@@ -17,4 +17,4 @@ apt-get install spotify-client
 # Signal
 curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
-sudo apt update && sudo apt install signal-desktop
+sudo apt update && sudo apt install -y signal-desktop
---
+++ parts/security.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# Remove ubuntu shopping lens and the annoying apport.
+sudo apt-get -y install pass
