c1be899fd0839374b7af81cd016336dcfc6a9b68
Fredric Bergström <fredric@bobodrone.com>
2018-01-26 14:17:38 +0100

    added keybase

--- parts/security.sh
+++ parts/security.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
 # Remove ubuntu shopping lens and the annoying apport.
-sudo apt-get -y install pass
+#sudo apt-get -y install pass
+
+curl -O https://prerelease.keybase.io/keybase_amd64.deb
+sudo dpkg -i keybase_amd64.deb
+sudo apt-get install -f -y
+rm keybase_amd64.deb
