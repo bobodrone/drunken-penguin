d1622eba5c5932cf8275a25a911d3ebaa567baf8
Fredric Bergström <fredric@bobodrone.com>
2018-01-27 08:14:38 +0100

    more stuff

--- parts/beautify.sh
+++ parts/beautify.sh
@@ -3,3 +3,8 @@
 sudo add-apt-repository -y ppa:papirus/papirus
 sudo apt-get update
 sudo apt-get -y install papirus-icon-theme gnome-tweak-tool arc-theme
+
+# install gnome-shell
+sudo add-apt-repository -y ppa:ne0sight/chrome-gnome-shell
+sudo apt-get update
+sudo apt-get -y install chrome-gnome-shell
--- parts/php.sh
+++ parts/php.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
 sudo apt-get -y install php-cli php-xml php-zip php-json php-mcrypt php-redis php-xdebug php-soap php-mbstring php-curl php-mysql php-codesniffer composer
+
+# install pingur
+cd
+git clone git@github.com:nodeone/pingur.git
+cd pingur
+composer install
+app/pingur
