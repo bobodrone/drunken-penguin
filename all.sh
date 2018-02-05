142cfc3caed2c73a9baec82927a50f099738aa01
Fredric Bergström <fredric.bergstrom@digitalistgroup.com>
2018-02-05 16:38:58 +0100

    asdafasdf

--- all.sh
+++ all.sh
@@ -3,13 +3,12 @@
 ./parts/cleanup.sh
 ./parts/essentials.sh
 ./parts/browsers.sh
-#./parts/emacs.sh
 ./parts/mysql.sh
 ./parts/php.sh
 ./parts/nodejs.sh
 ./parts/vagrant.sh
 ./parts/leisure.sh
-#./parts/docker.sh
+./parts/docker.sh
 ./parts/wp.sh
 ./parts/drupal.sh
 ./parts/vscode.sh
@@ -17,3 +16,5 @@
 # Shell needs to be (almost) last
 ./parts/shell.sh
 ./parts/python.sh
+./parts/security.sh
+./parts/ruby.sh
