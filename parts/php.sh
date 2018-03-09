5f573667ed55c4d764734a5aa3284a582e8033d1
Fredric Bergström <fredric.bergstrom@digitalistgroup.com>
2018-03-09 12:38:31 +0100

    added gd

--- parts/php.sh
+++ parts/php.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo apt-get -y install php-cli php-xml php-zip php-json php-mcrypt php-redis php-xdebug php-soap php-mbstring php-curl php-mysql php-codesniffer composer
+sudo apt-get -y install php-cli php-xml php-zip php-json php-mcrypt php-redis php-xdebug php-soap php-mbstring php-curl php-mysql php-codesniffer composer php-gd
 
 # install pingur
 cd
